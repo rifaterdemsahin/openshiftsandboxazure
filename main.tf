0ee7ad2bab7f5f079e658262b4e818573e2c01f0
rifaterdemsahin <rifaterdemsahin@hotmail.com>
2024-08-13 18:50:17 +0100

    push main

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 provider "azurerm" {
-  features = {}
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "openshiftsandbox" {
-  name     = "openshiftsandbox"
-  location = "UK South"
+  name     = var.resource_group_name
+  location = var.location
 }
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "location" {
+  type    = string
+  description = "The location/region where the resource group will be created"
+  default = "UK South"
+}
+variable "admin_username" {
+  description = "The admin username for the virtual machine"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the virtual machine"
+  type        = string
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription ID for Azure"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for Azure"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret for Azure"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for Azure"
+  type        = string
+}
